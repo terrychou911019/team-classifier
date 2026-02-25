6c5f2adcee9be8d18ba73a6879985fb7e92f60ff
TerryChou <terrychou911019@gmail.com>
2026-02-25 16:22:19 +0800

    refactor: update run_assign_teams.py

--- run_assign_teams.py
+++ run_assign_teams.py
@@ -1,22 +1,26 @@
 import subprocess
+import os
 
-print("Processing SportsMOT val")
-cmd = ["python", "assign_teams.py", 
-       "--data_path", "D:/CourtAthena/02_Datasets/sportsmot_publish/dataset/val", 
-       "--pred_dir", "D:/CourtAthena/03_Experiments/TrackEval/data/trackers/mot_challenge/SportsMOT-val/mcbyte+filter+interpolate/data", 
-       "--output_dir", "assign_teams_output/SportsMOT"]
-subprocess.run(cmd, check=True)
+team_classifier = "clipreid"
+os.makedirs(f"assign_teams_output/{team_classifier}/TGB", exist_ok=True)
 
-print("Processing TGB test ")
-cmd = ["python", "assign_teams.py", 
+# print("Processing SportsMOT val")
+# cmd = ["python", "assign_teams.py", 
+#        "--data_path", "D:/CourtAthena/02_Datasets/sportsmot_publish/dataset/val", 
+#        "--pred_dir", "D:/CourtAthena/03_Experiments/TrackEval/data/trackers/mot_challenge/SportsMOT-val/mcbyte+filter+interpolate/data", 
+#        "--output_dir", f"assign_teams_output/{team_classifier}/SportsMOT"]
+# subprocess.run(cmd, check=True)
+
+print("Processing TGB test")
+cmd = ["python", f"assign_teams_{team_classifier}.py", 
        "--data_path", "D:/CourtAthena/02_Datasets/TGB/dataset", 
-       "--pred_dir", "D:/CourtAthena/03_Experiments/TrackEval/data/trackers/mot_challenge/TGB-test/mcbyte+filter+interpolate/data", 
-       "--output_dir", "assign_teams_output/TGB"]
+       "--pred_dir", "D:/CourtAthena/03_Experiments/TrackEval/data/trackers/mot_challenge/TGB-test/mcbyte/data", 
+       "--output_dir", f"assign_teams_output/{team_classifier}/TGB"]
 subprocess.run(cmd, check=True)
 
-print("Processing TrackID3x3 test")
-cmd = ["python", "assign_teams.py", 
-       "--data_path", "D:/CourtAthena/02_Datasets/trackid3x3_720p/test", 
-       "--pred_dir", "D:/CourtAthena/03_Experiments/TrackEval/data/trackers/mot_challenge/TrackID3x3-test/mcbyte+filter+interpolate/data", 
-       "--output_dir", "assign_teams_output/TrackID3x3"]
-subprocess.run(cmd, check=True)
+# print("Processing TrackID3x3 test")
+# cmd = ["python", "assign_teams.py", 
+#        "--data_path", "D:/CourtAthena/02_Datasets/trackid3x3_720p/test", 
+#        "--pred_dir", "D:/CourtAthena/03_Experiments/TrackEval/data/trackers/mot_challenge/TrackID3x3-test/mcbyte+filter+interpolate/data", 
+#        "--output_dir", f"assign_teams_output/{team_classifier}/TrackID3x3"]
+# subprocess.run(cmd, check=True)
